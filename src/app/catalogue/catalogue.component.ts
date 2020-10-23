8f5e67d271f173d2048acf923731662536eaf03c
DaviDLmk <litzlerdavid@gmail.com>
2020-10-23 09:40:01 +0200

    3 files modified, 1 added

--- src/app/catalogue/catalogue.component.ts
+++ src/app/catalogue/catalogue.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Produit } from "../models/produit";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-catalogue',
-  templateUrl: './catalogue.component.html',
-  styleUrls: ['./catalogue.component.css']
+  selector: "app-catalogue",
+  templateUrl: "./catalogue.component.html",
+  styleUrls: ["./catalogue.component.css"]
 })
 export class CatalogueComponent implements OnInit {
+  produits: Observable<Produit[]>;
+  filterWord: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
